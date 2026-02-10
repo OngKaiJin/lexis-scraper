f2a024ec0cd2441219e8585166b51766c7ecc2b5
Ong Kai Jin <47915713+OngKaiJin@users.noreply.github.com>
2026-02-10 15:24:13 +0800

    Update listplain.js

--- listplain.js
+++ listplain.js
@@ -1,6 +1,6 @@
 command = '';
 for (const i of list) {
-    if (i.hasOwnProperty("pdf") && i.pdf != "--") {
+    if (i != undefined && i.hasOwnProperty("pdf") && i.pdf != "--") {
         command += ('' + i.pdf).slice(16) + '\n';
     }
 }
